9511ea1fa8e7a477b24012f720e62a8bb080a37f
Robin Kählert <robin@s-kaehlert.de>
2020-10-02 18:28:02 +0200

    Update deps

--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Recipe;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 
@@ -16,7 +16,7 @@ use Pagerfanta\Pagerfanta;
  */
 class RecipeRepository extends ServiceEntityRepository {
 
-    public function __construct(RegistryInterface $registry) {
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Recipe::class);
     }
 
